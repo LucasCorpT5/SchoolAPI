b5327422a423d04d01e3a138d2e2c5d74cf6d8b8
LucasCorpT5 <lucasaraujotda13@gmail.com>
2021-12-16 23:04:48 -0400

    temp_not_ob

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
+import prisma from "../prisma/index";
 
 class HomeController {
-  index(req: Request, res: Response) {
-    res.json({
-      tudoCerto: true
+  async index(req: Request, res: Response) {
+    const newStudent = await prisma.student.create({
+      data: {
+        name: "Lucas",
+        surname: "Araujo",
+        email: "Lucas@gmail.com",
+        age: 12,
+      }
     });
+    res.json(newStudent);
   }
 }
 
