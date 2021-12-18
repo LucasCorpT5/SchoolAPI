42c107753c87154200d32927f3291de74d4d3378
LucasCorpT5 <lucasaraujotda13@gmail.com>
2021-12-18 16:14:22 -0400

    first_verify

--- src/controllers/TokenController.ts
+++ src/controllers/TokenController.ts
@@ -3,7 +3,28 @@ import prisma from "../prisma/index";
 
 class TokenController {
   async create(req: Request, res: Response) {
-    res.json("OK");
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(401).json({
+        error: "Invalid email or password"
+      })
+    }
+    const user = await prisma.users.findUnique({
+      where: {
+        email: email,
+      }
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        error: "Invalid user"
+      })
+    } else {
+      return res.status(200).json({
+        message: "Test passed ✔",
+      });
+    }
   }
 }
 
